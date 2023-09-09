cffe3eca6025af35c09809d4b0181329d38bb465
Hashim Syed <hashimsyed@dhcp-206-87-208-5.ubcsecure.wireless.ubc.ca>
2023-09-08 18:22:28 -0700

    finished bargraph, still need tooltip started table

--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -1,1 +1,9 @@
-export const TEST = 'TEST';
+export const STAGE_NAMES = {
+    APPLIED: 'Applied',
+    INTERVIEW: 'Interview',
+    OA: 'Online Assessment',
+    OFFER: 'Offer',
+    REJECTED: 'Rejected',
+    TARGET: "Target",
+}
+
